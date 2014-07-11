dff9fefa3831963d91052ca4036ca97d765bb0a4
Fitz Ryland <fitzryland@gmail.com>
2014-07-11 12:55:08 -0700

    basic html boilerplate

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // JavaScript
+        concat: {
+            // 2. Configuration for concatinating files goes here.
+            dist: {
+                src: [
+                    'js/lib/*.js', // All JS in the libs folder
+                    'js/main.js'  // This specific file
+                ],
+                dest: '.tmp/production.js',
+            }
+        },
+        uglify: {
+            build: {
+                src: '.tmp/production.js',
+                dest: 'js/production.min.js'
+            }
+        },
+        // Style
+        compass: {
+            dist: {}
+        },
+        autoprefixer: {
+            dist: {
+                files: {
+                    'css/main.css': '.tmp/main.css'
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            css: {
+                files: ['css/sass/**/*.scss'],
+                tasks: ['compass', 'autoprefixer'],
+                options: {
+                    spawn: false,
+                }
+            },
+            scripts: {
+                files: ['js/**/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify', 'compass', 'autoprefixer']);
+
+};
